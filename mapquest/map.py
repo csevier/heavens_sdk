daa5a92e095a98160534bbed321845bcff8556ea
Cody Sever <sevierit@gmail.com>
2023-08-16 23:28:17 -0500

    issue with texture division at 0 because i dont load wads, but heres seemingly a working version

--- mapquest/map.py
+++ mapquest/map.py
@@ -4,7 +4,7 @@ from geo_generator import GeoGenerator
 
 mp = MapParser()
 # mp.parser_load("test.map")
-mp.parser_load("qodot_test.map")
+mp.parser_load("test_maps/qodot_test.map")
 # mp.parser_load("E1M1.MAP")
 mp.map_data.load_texture_data()
 geo_gen = GeoGenerator(mp.map_data)
